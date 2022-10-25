a75d398a11e463879fa70bc3b155072ccb0e8721
Nihad Baberović <nbaberovic1@etf.unsa.ba>
2022-10-25 23:21:56 +0200

    Dopuna main metode i poboljšanje funkcije sumaCifara

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static int sumaCifara(int n) {
             int suma = 0;
+            if(n<0) n = -n;
             while(n != 0) {
                 suma += n%10;
                 n /= 10;
@@ -15,8 +16,9 @@ public class Main {
         Scanner ulaz = new Scanner(System.in);
         System.out.println("Unesite broj n: ");
         n = ulaz.nextInt();
+        if(n < 1) System.out.println("Uneseni broj je manji od 1!");
         for(int i=1; i <= n; i++) {
-            if( (i % sumaCifara(i)) == 0) System.out.println(i);
+            if ((i % sumaCifara(i)) == 0) System.out.println(i);
         }
     }
 }
